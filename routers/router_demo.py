b7169ea299a083b3a4df2f20f8dceee0b61663ee
wclin <7461827@qq.com>
2024-05-30 14:07:09 +0800

    t

--- routers/router_demo.py
+++ routers/router_demo.py
@@ -41,5 +41,5 @@ def fetch_messages_by_session_id(session_id: str, session: Session = Depends(DbE
   
     statement = select(Chat_history_new.message).where(Chat_history_new.session_id == session_id)
     result = session.exec(statement).all()
-    messages = [row[0] for row in result]
+    messages = [row for row in result]
     return messages
